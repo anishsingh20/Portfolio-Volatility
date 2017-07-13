abd7486bdb7aac4b3ddbef121cefce39c347d462
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-13 19:25:37 +0530

    File created and added:

---
+++ portfolio.R
@@ -0,0 +1,3 @@
+#Import prices and calculate returns for 5 assets and construct a portfolio.
+#Calculate the standard deviation of monthly portfolio returns using three methods:
+#Compare those to the standard deviation of monthly SPY returns.
